e69358526d5eb5b9d3e82a5924cf7afba9da6f51
Ted M. Young <tedyoung@gmail.com>
2022-08-15 10:38:52 -0700

    LAB #1: Add characterization test for displaying TEN-ranked Card

---
+++ src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
@@ -0,0 +1,16 @@
+package com.jitterted.ebp.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CardDisplayTest {
+
+    @Test
+    public void displayTenAsString() throws Exception {
+        Card card = new Card(Suit.CLUBS, Rank.TEN);
+
+        assertThat(card.display())
+                .isEqualTo("[30m┌─────────┐[1B[11D│10       │[1B[11D│         │[1B[11D│    ♣    │[1B[11D│         │[1B[11D│       10│[1B[11D└─────────┘");
+    }
+}
