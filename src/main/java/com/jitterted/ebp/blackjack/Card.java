674e2085a7a5aa4f53c3cdbd1dce23c3a8bb261e
Ted M. Young <tedyoung@gmail.com>
2022-08-15 10:48:06 -0700

    LAB #1: Extracted suit and rank to query methods

--- src/main/java/com/jitterted/ebp/blackjack/Card.java
+++ src/main/java/com/jitterted/ebp/blackjack/Card.java
@@ -20,19 +20,28 @@ public class Card {
     public String display() {
         String[] lines = new String[7];
         lines[0] = "┌─────────┐";
-        lines[1] = String.format("│%s%s       │", rank.display(), rank == Rank.TEN ? "" : " ");
+        lines[1] = String.format("│%s%s       │", rank().display(), rank() == Rank.TEN ? "" : " ");
         lines[2] = "│         │";
-        lines[3] = String.format("│    %s    │", suit.symbol());
+        lines[3] = String.format("│    %s    │", suit().symbol());
         lines[4] = "│         │";
-        lines[5] = String.format("│       %s%s│", rank == Rank.TEN ? "" : " ", rank.display());
+        lines[5] = String.format("│       %s%s│", rank() == Rank.TEN ? "" : " ", rank().display());
         lines[6] = "└─────────┘";
 
-        Ansi.Color cardColor = suit.isRed() ? Ansi.Color.RED : Ansi.Color.BLACK;
+        Ansi.Color cardColor = suit().isRed() ? Ansi.Color.RED : Ansi.Color.BLACK;
         return ansi()
                 .fg(cardColor).toString()
                 + String.join(ansi().cursorDown(1)
                                     .cursorLeft(11)
                                     .toString(), lines);
+    }
+
+    public Suit suit() {
+        return suit;
+    }
+
+    // Returns immutable object
+    public Rank rank() {
+        return rank;
     }
 
     @Override
