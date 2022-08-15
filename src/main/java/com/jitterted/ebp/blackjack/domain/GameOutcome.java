c0d8ec223d692fcae813f856274a198c489d024f
Ted M. Young <tedyoung@gmail.com>
2022-08-15 16:33:18 -0700

    LAB #4: Extracted Enum (via Introduce Constant and Extract Delegate). Had to fix up tests manually.

---
+++ src/main/java/com/jitterted/ebp/blackjack/domain/GameOutcome.java
@@ -0,0 +1,20 @@
+package com.jitterted.ebp.blackjack.domain;
+
+public enum GameOutcome {
+    PLAYER_BUSTED("You Busted, so you lose.  💸"),
+    DEALER_BUSTED("Dealer went BUST, Player wins! Yay for you!! 💵"),
+    PLAYER_BEATS_DEALER("You beat the Dealer! 💵"),
+    PLAYER_PUSHES("Push: Nobody wins, we'll call it even."),
+    PLAYER_LOSES("You lost to the Dealer. 💸");
+
+    private String value;
+
+    @Deprecated // this is I/O specific
+    public String display() {
+        return value;
+    }
+
+    GameOutcome(String value) {
+        this.value = value;
+    }
+}
