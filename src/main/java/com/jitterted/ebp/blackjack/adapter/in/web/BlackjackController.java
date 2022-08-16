6c18b646844559aa234b2b6322309c90bac72654
Ted M. Young <tedyoung@gmail.com>
2022-08-16 10:34:48 -0700

    TDD: Unit Test: start game now does initial deal in Game

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
@@ -1,13 +1,22 @@
 package com.jitterted.ebp.blackjack.adapter.in.web;
 
+import com.jitterted.ebp.blackjack.domain.Game;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
+// ADAPTER Controller
 @Controller
 public class BlackjackController {
 
+    private Game game;
+
+    public BlackjackController(Game game) {
+        this.game = game;
+    }
+
     @PostMapping("/start-game")
     public String startGame() {
+        game.initialDeal();
         return "redirect:/";
     }
 
---
+++ src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackControllerTest.java
@@ -0,0 +1,22 @@
+package com.jitterted.ebp.blackjack.adapter.in.web;
+
+import com.jitterted.ebp.blackjack.domain.Deck;
+import com.jitterted.ebp.blackjack.domain.Game;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class BlackjackControllerTest {
+
+    @Test
+    public void startGameThenTwoCardsDealtToPlayer() throws Exception {
+        Game game = new Game(new Deck());
+        BlackjackController blackjackController = new BlackjackController(game);
+
+        blackjackController.startGame();
+
+        assertThat(game.playerHand().cards())
+                .hasSize(2);
+    }
+
+}
