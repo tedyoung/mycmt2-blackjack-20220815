ada488582f0c186d63133abb31900aec04c4e21b
Ted M. Young <tedyoung@gmail.com>
2022-08-16 14:07:33 -0700

    LAB #7: Add ArchUnit test and fix (test code) violation of Hexagonal Architecture

--- src/test/java/com/jitterted/ebp/blackjack/adapter/in/console/CardDisplayTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/adapter/in/console/CardDisplayTest.java
@@ -6,8 +6,11 @@ import com.jitterted.ebp.blackjack.domain.Suit;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.fusesource.jansi.Ansi.ansi;
 
 public class CardDisplayTest {
+
+    private static final Rank DUMMY_RANK = Rank.TEN;
 
     @Test
     public void displayTenAsString() throws Exception {
@@ -24,4 +27,21 @@ public class CardDisplayTest {
         assertThat(ConsoleCard.display(card))
                 .isEqualTo("[30m┌─────────┐[1B[11D│2        │[1B[11D│         │[1B[11D│    ♠    │[1B[11D│         │[1B[11D│        2│[1B[11D└─────────┘");
     }
+
+    @Test
+    public void suitOfHeartsOrDiamondsIsDisplayedInRed() throws Exception {
+        // given a card with Hearts or Diamonds
+        Card heartsCard = new Card(Suit.HEARTS, DUMMY_RANK);
+        Card diamondsCard = new Card(Suit.DIAMONDS, DUMMY_RANK);
+
+        // when we ask for its display representation
+        String ansiRedString = ansi().fgRed().toString();
+
+        // then we expect a red color ansi sequence
+        assertThat(ConsoleCard.display(heartsCard))
+                .contains(ansiRedString);
+        assertThat(ConsoleCard.display(diamondsCard))
+                .contains(ansiRedString);
+    }
+
 }
