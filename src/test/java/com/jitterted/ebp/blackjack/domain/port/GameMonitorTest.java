ccc4616574f2dcee56d6d67d5347b0301c0cf330
Ted M. Young <tedyoung@gmail.com>
2022-08-16 15:46:39 -0700

    LAB #8: Added GameMonitor as argument to 2nd constructor for Game. First test scenario of player stands calling game monitor passes.

---
+++ src/test/java/com/jitterted/ebp/blackjack/domain/port/GameMonitorTest.java
@@ -0,0 +1,25 @@
+package com.jitterted.ebp.blackjack.domain.port;
+
+import com.jitterted.ebp.blackjack.domain.Game;
+import com.jitterted.ebp.blackjack.domain.StubDeck;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+
+public class GameMonitorTest {
+
+    @Test
+    public void playerStandsThenGameIsOverAndResultsSentToMonitor() throws Exception {
+        GameMonitor gameMonitorSpy = spy(GameMonitor.class);
+        Game game = new Game(StubDeck.playerStandsAndBeatsDealer(), gameMonitorSpy);
+        game.initialDeal();
+
+        game.playerStands();
+
+        verify(gameMonitorSpy).roundCompleted(any(Game.class));
+    }
+
+}
